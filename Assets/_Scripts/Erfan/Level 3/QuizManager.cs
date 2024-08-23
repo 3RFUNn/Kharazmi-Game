6bb0ad8b047316a66dfd8edf197f881cd1310353
3RFUNn <68325981+3RFUNn@users.noreply.github.com>
2024-08-23 17:46:00 +0330

    Update QuizManager.cs

--- Assets/_Scripts/Erfan/Level 3/QuizManager.cs
+++ Assets/_Scripts/Erfan/Level 3/QuizManager.cs
@@ -54,9 +54,7 @@ public class QuizManager : MonoBehaviour
         {
             int k = Random.Range(0,
                 n--);
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
+            (list[k], list[n]) = (list[n], list[k]);
         }
     }
 
