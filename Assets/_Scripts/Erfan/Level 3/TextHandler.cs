772d1a0ba7c502de42ef858f4ddaa0be83e40575
3RFUNn <68325981+3RFUNn@users.noreply.github.com>
2024-09-04 22:04:17 +0330

    changes

--- Assets/_Scripts/Erfan/Level 3/TextHandler.cs
+++ Assets/_Scripts/Erfan/Level 3/TextHandler.cs
@@ -10,40 +10,24 @@ using UnityEngine;
 public class TextHandler : MonoBehaviour
 {
     // Reference to RTLTextMeshPro components
-    public RTLTextMeshPro rtlText1;
-    public RTLTextMeshPro rtlText2;
-    public RTLTextMeshPro rtlText3;
-    public RTLTextMeshPro rtlText4;
+    public RTLTextMeshPro[] rtlText;
+    
 
     // String fields to manually input text
     [TextArea(3, 10)]
-    public string text1;
-    [TextArea(3, 10)]
-    public string text2;
-    [TextArea(3, 10)]
-    public string text3;
-    [TextArea(3, 10)]
-    public string text4;
+    public string[] text1;
+    
 
     // This function will be called whenever the script is updated in the editor
     void OnValidate()
     {
         // Check if all text components are assigned and set the corresponding text
-        if (rtlText1 != null)
+        for (int i = 0; i < rtlText.Length; i++)
         {
-            rtlText1.text = text1;
-        }
-        if (rtlText2 != null)
-        {
-            rtlText2.text = text2;
-        }
-        if (rtlText3 != null)
-        {
-            rtlText3.text = text3;
-        }
-        if (rtlText4 != null)
-        {
-            rtlText4.text = text4;
+            if (rtlText[i] != null)
+            {
+                rtlText[i].text = text1[i];
+            }
         }
     }
 }
