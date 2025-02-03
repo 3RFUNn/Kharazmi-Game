297dcc5395312d824613ac77f96ea6055a24fd6a
ARASH\arash <arashrajabpour01@gmail.com>
2025-02-03 18:23:48 +0330

    [add] added profile manager

---
+++ Assets/_Scripts/Arash/ProfileManager.cs
@@ -0,0 +1,40 @@
+using RTLTMPro;
+using UnityEngine;
+
+public class ProfileManager : SingletonBehaviour<ProfileManager>
+{
+    [SerializeField] public RTLTextMeshPro classRank;
+    [SerializeField] public RTLTextMeshPro globalRank;
+    [SerializeField] public RTLTextMeshPro points;
+    [SerializeField] public RTLTextMeshPro difficultyPointsText;
+
+    [SerializeField] public RectTransform level1Rect;
+    [SerializeField] public RectTransform level2Rect;
+    [SerializeField] public RectTransform level3Rect;
+    [SerializeField] public RectTransform level4Rect;
+    [SerializeField] RectTransform recordRectTransform;
+    
+    public void SetSize(int level,int percent)
+    {
+        float maxHeight = recordRectTransform.sizeDelta.y;
+        switch (level) {
+            case 1:
+                level1Rect.sizeDelta = new Vector2(level1Rect.sizeDelta.x, maxHeight * percent / 100);
+                break;
+            case 2:
+                level2Rect.sizeDelta = new Vector2(level2Rect.sizeDelta.x, maxHeight * percent / 100);
+                break;
+            case 3:
+                level3Rect.sizeDelta = new Vector2(level3Rect.sizeDelta.x, maxHeight * percent / 100);
+                break;
+            case 4:
+                level4Rect.sizeDelta = new Vector2(level4Rect.sizeDelta.x, maxHeight * percent / 100);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+}
