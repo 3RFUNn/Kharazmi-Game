5d22091bfb82a4b401d263b0a6d86ddc7cb0e979
Kamran Miralami <kamran.m1380@yahoo.com>
2025-03-04 00:15:14 +0330

    [fix] small adjustment with tutorial manager

--- Assets/_Scripts/Kamran/HeaderHandler.cs
+++ Assets/_Scripts/Kamran/HeaderHandler.cs
@@ -9,7 +9,7 @@ public class HeaderHandler : MonoBehaviour
     [SerializeField] int level;
     private void Start()
     {
-        infoBtn.onClick.AddListener(()=>TutorialManager.Instance.OpenTutorial(level));
+        infoBtn.onClick.AddListener(()=>TutorialManager.Instance.OpenTutorial());
         MapBtn.onClick.AddListener(() => RoadMapManager.Instance.OpenRoadMap(level));
     }
     public void SetTimeObjectEnable(bool val)
